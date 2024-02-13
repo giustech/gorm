f643c6823a30a34079c343044944513bc701d63c
Guilherme Giusti <guilherme.giusti04@gmail.com>
2024-02-12 23:16:12 -0300

    check repo

--- pkg/database/sqlite.go
+++ pkg/database/sqlite.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"os"
 	"strings"
 	"time"
 )
@@ -24,5 +25,22 @@ func (sqliteConnection) GetInstance(_ string, _ string, _ string, _ int, _ strin
 	now := time.Now()
 	timestampStr := now.Format("2006-01-02 15:04:05")
 	sql := fmt.Sprintf("sqlite/%s.db", strings.Replace(strings.ReplaceAll(strings.ReplaceAll(timestampStr, "-", ""), ":", ""), " ", "", 1))
+	if !checkIfFolderExists("./sqlite") {
+		fmt.Println("Folder sqlite must be created in test context repository")
+	}
 	return gorm.Open(sqlite.Open(sql), &config)
+}
+
+func checkIfFolderExists(folderPath string) bool {
+	info, err := os.Stat(folderPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			// A pasta não existe
+			return false
+		}
+		// Houve algum outro erro ao tentar obter as informações
+		return false
+	}
+	// Verifica se o caminho é de fato uma pasta (e não um arquivo)
+	return info.IsDir()
 }
