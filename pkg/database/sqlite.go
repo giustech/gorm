a49339e3c8abc1a97b6b7116cc2ebc2f9ece120e
Guilherme Giusti <guilherme.giusti04@gmail.com>
2024-02-14 02:02:39 -0300

    update sqlite show sql

--- pkg/database/sqlite.go
+++ pkg/database/sqlite.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+	"log"
 	"os"
 	"strings"
 	"time"
@@ -24,6 +27,23 @@ func (sqliteConnection) GetInstance(_ string, _ string, _ string, _ int, _ strin
 	var config gorm.Config
 	now := time.Now()
 	timestampStr := now.Format("2006-01-02 15:04:05")
+
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold: time.Second, // Limiar de tempo lento para consultas SQL
+			LogLevel:      logger.Info, // LogLevel Info irá imprimir todas as consultas SQL
+			Colorful:      true,        // Habilita a cor no log para uma melhor distinção
+		},
+	)
+	config = gorm.Config{
+		Logger: newLogger,
+		NamingStrategy: schema.NamingStrategy{
+			TablePrefix:   "entities.",
+			SingularTable: false,
+		},
+	}
+
 	sql := fmt.Sprintf("sqlite/%s.db", strings.Replace(strings.ReplaceAll(strings.ReplaceAll(timestampStr, "-", ""), ":", ""), " ", "", 1))
 	if !checkIfFolderExists("./sqlite") {
 		fmt.Println("Folder sqlite must be created in test context repository")
